c6ee2554fdfffa439503b1bab719ed43368c3888
Pieter Jan De Smedt <pieterjan@digitalcomet.be>
2017-02-13 21:36:34 +0100

    fix: remove bcrypt dependency (use bcryptjs if needed)

--- server.js
+++ server.js
@@ -186,8 +186,8 @@ if (process.env.NODE_ENV === 'development') {
 	app.use(errorhandler());
 }
 
-app.use(require('./anonymous-routes'));
-app.use(require('./protected-routes'));
+// app.use(require('./anonymous-routes'));
+// app.use(require('./protected-routes'));
 
 const port = process.env.PORT || 3001;
 
